771867768e2c2ea9895bd57191eb73c8eb3c0ea4
lvprasadv <84435748+lvprasadv@users.noreply.github.com>
2023-01-25 15:20:05 +0530

    Update oc-tenable-centos.tf

--- oc-tenable-centos.tf
+++ oc-tenable-centos.tf
@@ -65,7 +65,21 @@ resource "google_os_config_os_policy_assignment" "oc-tenable" {
             }         
           }
        }
-     
+
+      resources {
+        id = "validate-nessus-status"
+        exec {
+          validate {
+            interpreter = "SHELL"
+            script      = "if [[ $(service nessusagent status | grep 'active (running') ]]; then echo 'Nessus Agent is Installed, State is running - Link Status Check Required'; exit 100; else exit 101; fi"
+          }
+
+          enforce {
+            interpreter = "SHELL"
+            script      = "if [[ $(/opt/nessus_agent/sbin/nessuscli agent status | grep 'error') || $(/opt/nessus_agent/sbin/nessuscli agent status | grep 'warn') || $(/opt/nessus_agent/sbin/nessuscli agent status | grep 'Not linked')]]; then echo 'Nessus Agent is not linked properly. Linking the agent'; nessuskey='a521b5ff16a5d5272109d675bba8d84bd07e7126d686c1966ec8e1fce13abd16'; NessusGroup='gcp-oc-dev'; /opt/nessus_agent/sbin/nessuscli agent link --port=443 --key=$nessuskey --groups=''$NessusGroup''; "
+            }         
+          }
+       }     
      }
     }
 
